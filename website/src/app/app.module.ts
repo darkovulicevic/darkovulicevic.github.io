a15fe4f6ad7e87f90f7af91ab4d72549e9a3cf82
Darko Vulicevic <darko.vulicevic@enreach.com>
2024-12-16 01:48:46 +0100

    html, tailwind confings

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
