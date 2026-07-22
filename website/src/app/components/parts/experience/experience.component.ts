0625c4d67063e5f94036c9c79825e079ee819454
Darko Vulicevic <darko.vulicevic@enreach.com>
2026-07-23 00:13:33 +0200

    position

--- website/src/app/components/parts/experience/experience.component.ts
+++ website/src/app/components/parts/experience/experience.component.ts
@@ -21,7 +21,7 @@ export class ExperienceComponent {
         cards: <TExperienceCard[]>[
             {
                 date: 'July 2026 - Present',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Currently involved in the process of signing and notarizing native applications for both macOS and Windows platforms.',
                 skills: [
                     { name: 'Code Signing' },
@@ -33,7 +33,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'March 2026 - now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Currently involved in the development of two additional React applications focused on configuration management and consolidation of shared entities across multiple applications.',
                 skills: [
                     { name: 'JavaScript' },
@@ -45,7 +45,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Jan 2026 - now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Actively participating in the architectural decision-making process through TAB (Technical Alignment Board) meetings, contributing to the evaluation and selection of architectural approaches, frameworks, and technical standards across projects.',
                 skills: [
                     { name: 'System Design' },
@@ -62,7 +62,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'June 2025 - now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to and currently continuing the development of a complex scheduling feature within the Reporting Portal, enabling users to configure one-time and recurring report generation with flexible time periods, intervals, and recurrence ranges. The feature supports a wide variety of scheduling scenarios including daily, weekly, and monthly recurrences with configurable start times, end conditions, and delivery options.',
                 skills: [
                     { name: 'JavaScript' },
@@ -75,7 +75,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Jan 2025 - now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to and currently maintaining Nginx server, overseeing domain name configurations and updates. Played a key role in developing a custom proxy server Squid through Enreach Contact apps, ensuring efficient traffic routing, security, and performance. Continuously optimizing configurations to enhance reliability and support seamless web services.',
                 skills: [
                     { name: 'Linux/Unix' },
@@ -88,7 +88,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2024 - now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Actively contributing to the Reporting Portal project — a React(v19) web application that offers tools for managing and visualizing reports and statistics.',
                 skills: [
                     { name: 'JavaScript' },
@@ -101,7 +101,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2023 – Now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to the implementation of Drone CI/CD pipelines across Enreach Contact and QBOARD app, and currently responsible for maintaining and refining build scripts to ensure seamless integration, efficient deployment, and continuous improvement of the development workflow.',
                 skills: [
                     { name: 'Drone' },
@@ -113,7 +113,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2022 – Now',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Utilizing and maintaining a custom component and brand library across Enreact Contact, QBOARD, and the Reporting Portal, ensuring consistency, scalability, and alignment with design standards.',
                 skills: [
                     { name: 'JavaScript' },
@@ -124,7 +124,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2022 – NOW',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Continued to work on Enreach Contact (formerly Unified Desktop and Web Client) and the QBOARD web app. Responsible for developing new features to enhance functionality and improve user experience. Additionally, performing regular maintenance tasks, including identifying and resolving bugs, to ensure the applications remain robust and reliable.',
                 skills: [
                     { name: 'JavaScript' },
@@ -142,7 +142,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2022 – Jan 2023',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to the migration of Enreach Contact (formerly Unified Desktop and Web Client) and the QBOARD web app to a new backend system, Grid, aimed at enhancing scalability and overall performance. The migration improved the backend infrastructure, ensuring better support for both current and future needs.',
                 skills: [
                     { name: 'JavaScript' },
@@ -160,7 +160,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'May 2021 – Dec 2022',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to the development of Unified Desktop and Web Client, a real-time chat and call application built with Angular, focusing on delivering a seamless user experience across desktop and web platforms. Continued to work on QBOARD, enhancing functionalities and developing new features to improve performance, maintain the application, and identify various bugs. Contributed to the development of the Infinity JS Connection Library, Google JS Connection Library, Microsoft JS Connection Library, and Chat JS Library(Chat Library for Infinity backend) designed to connect to Infinity, Google, and Microsoft while utilizing various backend features. Each feature is represented by a single feature object, with helpful sub-objects created in certain cases to represent more complex items, such as soft phone calls or queues.',
                 skills: [
                     { name: 'JavaScript' },
@@ -178,7 +178,7 @@ export class ExperienceComponent {
             },
             {
                 date: 'Dec 2020 – May 2021',
-                pos: 'Frontend Engineer',
+                pos: 'JavaScript Developer',
                 desc: 'Contributed to the development of QBOARD, a web application in Angular for the support team to monitor busy and over-occupied lines. The project featured a refreshed UI based on the functionality of an older app and improved team efficiency and response times through enhanced usability.',
                 skills: [
                     { name: 'JavaScript' },
